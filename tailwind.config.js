fa0d22328edd1e1c24d505e01e8f86f4d4122bc5
Jasima yasmin <jasimayasmin78@gmail.com>
2025-11-21 16:26:40 -1200

    Added grid_align.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily:{
+      bebas: ['Bebas Neue','sans-serif']
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base:'1rem',
+      xl:'1.25rem',
+      '2xl':'1.563rem',
+      '3xl':'1.953rem',
+      '4xl':'2.441rem',
+      '5xl':'3.765rem',
+
+    }
   },
   plugins: [],
 }
